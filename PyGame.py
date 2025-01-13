29140749118b0800bc5d8216ece0c80e8bd07bd9
Vano <ingwar08ru@gmail.com>
2025-01-13 09:07:51 +0400

    Тут враг и файлы, но враг стремно реагирует на движение героя

--- PyGame.py
+++ PyGame.py
@@ -1,7 +1,8 @@
+from random import randint
+
 import pygame as pg
 
 
-# from random import randint
 class MainCharacter(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -9,13 +10,38 @@ class MainCharacter(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         colorkey = self.image.get_at((0, 0))
         self.image.set_colorkey(colorkey)
+        self.land = [[], [], []]
+        self.la_x = 0
+        self.la_y = 0
+        for i in range(3):
+            a = []
+            for j in range(3):
+                field = pg.sprite.Sprite()
+                field_image = pg.image.load('check_field.png')
+                field.image = field_image
+                field.rect = field.image.get_rect()
+                field.rect.x = 1000 * i - 1000
+                field.rect.y = 1000 * j - 1000
+                a.append(field)
+            self.land[i] = a
 
 
-def movement(key):
-    if chr(key) in move_letters.keys():
-        return move_letters[chr(key)]
-    else:
-        return [0, 0]
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pg.image.load('enemy.png')
+        self.rect = self.image.get_rect()
+        colorkey = self.image.get_at((0, 0))
+        self.image.set_colorkey(colorkey)
+        self.rect.x, self.rect.y = randint(0, 970), randint(0, 950)
+        distance = ((500 - self.rect.x) ** 2 + (500 - self.rect.y) ** 2) ** 0.5
+        self.speed = [int((500 - self.rect.x) / (distance / (speed - 2))), int((500 - self.rect.y) / (distance / (speed - 2)))]
+
+    def update(self):
+        self.rect.x += self.speed[0]
+        self.rect.y += self.speed[1]
+        distance = ((500 - self.rect.x) ** 2 + (500 - self.rect.y) ** 2) ** 0.5
+        self.speed = [int((500 - self.rect.x) / (distance / (speed - 2))), int((500 - self.rect.y) / (distance / (speed - 2)))]
 
 
 if __name__ == '__main__':
@@ -27,7 +53,7 @@ if __name__ == '__main__':
 
     move_up, move_down, move_left, move_right = False, False, False, False
     tp_l, tp_r, tp_u, tp_d = False, False, False, False
-    speed = 15
+    speed = 7
 
     main_char = MainCharacter()
     land = [[], [], []]
@@ -49,6 +75,10 @@ if __name__ == '__main__':
     count = 1
     clock = pg.time.Clock()
     running = True
+
+    enemies = pg.sprite.Group()
+    enemy = Enemy()
+    enemies.add(enemy)
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -76,6 +106,9 @@ if __name__ == '__main__':
             for j in i:
                 j.rect.x -= speed * int(move_right) - speed * int(move_left)
                 j.rect.y -= speed * int(move_down) - speed * int(move_up)
+                for k in enemies:
+                    k.rect.x -= speed * int(move_right) - speed * int(move_left)
+                    k.rect.y -= speed * int(move_down) - speed * int(move_up)
 
         screen.fill('black')
         if land[1][1].rect.x >= 1000:
@@ -95,7 +128,8 @@ if __name__ == '__main__':
             for j in i:
                 screen.blit(j.image, (j.rect.x, j.rect.y))
         screen.blit(main_char.image, (width // 2 - main_char.rect.width // 2, height // 2 - main_char.rect.height // 2))
-
+        enemies.update()
+        enemies.draw(screen)
         pg.display.flip()
         clock.tick(fps)
     pg.quit()
